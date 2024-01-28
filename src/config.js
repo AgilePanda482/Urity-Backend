6ad19dec22e6261406f043d8e0fb69366d0c9ff1
David Vela <dsrv06@gmail.com>
2024-01-28 02:02:39 -0600

    adding database functionality

---
+++ src/config.js
@@ -0,0 +1,10 @@
+import { config } from "dotenv"
+config()
+
+export const variables = {
+    MYSQLDATABASE: process.env.MYSQLDATABASE,
+    MYSQLHOST: process.env.MYSQLHOST,
+    MYSQLPORT: process.env.MYSQLPORT,
+    MYSQLUSER: process.env.MYSQLUSER,
+    MYSQL_ROOT_PASSWORD: process.env.MYSQL_ROOT_PASSWORD
+}
