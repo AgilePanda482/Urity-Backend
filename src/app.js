751cfcdd6fc96f05581b77d797c498295a2d7e7d
David Vela <dsrv06@gmail.com>
2024-02-03 21:29:15 -0600

    connecting websocket with database

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import express from "express"
+import path from "path"
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, "..", "public")))
+
 
 export default app
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import app from "./app"
 import {Server as websocketServer} from "socket.io"
 import http from "http"
 import { pool } from "./db"
+import sockets from "./sockets"
 
 app.get("/ping", async (req, res) => {
-    const [rows] = await pool.query("select * from alumnos")
+    const [rows, fields] = await pool.query("select * from alumnos")
     console.log(rows)
     res.json(rows)
 })
@@ -16,6 +17,7 @@ console.log("Express escuchando en el puerto 4000")
 const server = http.createServer(app)
 const httpServer = server.listen(3000)
 const io = new websocketServer(httpServer)
+sockets(io)
 
 
 console.log("Server is running on port: 3000")
