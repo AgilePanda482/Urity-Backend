32fccdf042a9785ede4a407c4b91ae4dfbcbbee3
David Vela <dsrv06@gmail.com>
2024-03-22 12:50:07 -0600

    Fix interval time

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import path from "path"
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, "..", "public")))
+//app.use(express.static(path.join(__dirname, "..", "public")))
 
 
 export default app
