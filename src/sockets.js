196a39c6da067c0d6062d734bfa4c9b68f35279f
David Vela <dsrv06@gmail.com>
2024-04-02 00:27:06 -0600

    added verify socket

--- src/sockets.js
+++ src/sockets.js
@@ -18,9 +18,43 @@ export default (io) => {
 
     const intervalId = setInterval(async () => {
       const data = await characterData();
-      socket.emit('UID', data);
+      io.emit('UID', data);
       //console.log(data);
     }, 10000);
+
+    socket.on('verify', async (data) => {
+      //{veryify: true}
+      if (data.verify) {
+        // Simular una respuesta correcta o incorrecta de la base de datos
+        const simulateDBResponse = Math.random() < 0.5; // 50% de probabilidad
+  
+        if (simulateDBResponse) {
+          // Simular una consulta correcta a la base de datos
+          const [result] = await pool.query('SELECT * FROM alumno WHERE UID = 043E3B0F1D5480');
+          io.emit('verifyUIDFromArduino', result[0]);
+        } else {
+          // Simular una consulta incorrecta a la base de datos
+          io.emit('verifyUIDFromArduino', { error: 'USUARIO NO ENCONTRADO' });
+        }
+      }
+    });
+    
+    /*socket.on("verifyUIDFromArduino", async (data) => {
+      try {
+        // Realizar la consulta de forma asíncrona
+        const [result] = await pool.query("SELECT * FROM alumno WHERE UID = ?", [data.UID]);
+        if (result.length === 0) {
+          return socket.emit("UIDFromArduino", { UID: "USUARIO NO ENCONTRADO" });
+        }
+
+        socket.emit("UIDFromArduino", result[0]);
+      } catch (error) {
+        // Manejar cualquier error que ocurra durante la consulta
+        console.error(error);
+        socket.emit("UIDFromArduino", { error: "Error al realizar la consulta" });
+      }
+    });*/
+    
 
     socket.on('disconnect', () => {
       console.log('desconectado');
