0fec72beed75a123a6ce6da0530f0b02d7405fbe
David Vela <dsrv06@gmail.com>
2024-04-13 10:41:27 -0600

    added UIDTarjeta query on getAllUsers

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -3,48 +3,47 @@ import { transformarDatosArray } from "../libs/mapingData.js";
 let UIDTarjeta
 
 export const createUser = async (req, res) => {
-    try{
+    try {
         const {collegeCode, name, grade, group, career, shift, cardUID} = req.body;
         const [rows] = await pool.query("INSERT INTO alumnos (codigo, UIDTarjeta, nombres, grado, grupo, carrera, turno) VALUES (?, ?, ?, ?, ?, ?, ?)", [collegeCode, cardUID, name, grade, group, career, shift]);
         await pool.query("INSERT INTO estadoAlumnos (UIDTarjeta, localizacionAlumno, estadoInstitucional) VALUES (?, ?, ?)", [cardUID, 1, 1]);
 
         res.status(201).json({message: "User created successfully"});
-    }catch(error){
+    } catch(error) {
         console.log(error);
         return res.status(500).json({message: "Internal server error"});
     }
 }
 
 export const getAllUsers = async (req, res) => {
-    try{
-        const [rows] = await pool.query("SELECT a.codigo, a.nombres, a.carrera, e.localizacionAlumno FROM alumnos a JOIN estadoAlumnos e ON a.UIDTarjeta = e.UIDTarjeta;");
+    try {
+        const [rows] = await pool.query("SELECT a.codigo, a.nombres, a.carrera, a.UIDTarjeta, e.localizacionAlumno FROM alumnos a JOIN estadoAlumnos e ON a.UIDTarjeta = e.UIDTarjeta;");
         const arrayTransformado = transformarDatosArray(rows);
         res.status(200).json(arrayTransformado);
 
-    }catch(error){
+    } catch(error) {
         console.log(error);
         return res.status(500).json({message: "Internal server error"});
     }
 }
 
 export const getAnUser = async (req, res) => {
-    try{
+    try {
         const [rows] = await pool.query("SELECT a.nombres, a.codigo, a.grado, a.grupo, a.carrera, a.turno, a.UIDTarjeta, e.localizacionAlumno, e.estadoInstitucional FROM alumnos a JOIN estadoAlumnos e ON a.UIDTarjeta = e.UIDTarjeta WHERE a.UIDTarjeta = ?;", [req.params.id]);
         UIDTarjeta = rows[0].UIDTarjeta;
         
-
         if(rows.length <= 0){
             return res.status(404).json({message: "User not found"});
         }
         res.status(200).json(rows[0]);
-    }catch(error){
+    } catch(error) {
         console.log(error);
         return res.status(500).json({message: "Internal server error"});
     }
 }
 
 export const updateUser = async (req, res) => {
-    try{
+    try {
         const {collegeCode, name, grade, group, career, shift, cardUID} = req.body;
         const {location, status} = req.body;
 
@@ -53,7 +52,7 @@ export const updateUser = async (req, res) => {
 
         res.status(200).json({message: "User updated successfully"});
         UIDTarjeta = null;
-    }catch(error){
+    } catch(error) {
         console.log(error);
         return res.status(500).json({message: "Internal server error"});
     }	
