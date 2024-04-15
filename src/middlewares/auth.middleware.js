979b6e7f84d1ccd2c9b5552793c8e0fd99f3874d
David Vela <dsrv06@gmail.com>
2024-04-15 10:41:32 -0600

    added headers on middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,22 +1,26 @@
-import jwt from "jsonwebtoken"
-import { variables } from "../config"
+import jwt from "jsonwebtoken";
+import { variables } from "../config";
 
 export const verifyAuth = (req, res, next) => {
-    try{    
-        const { token } = req.cookies
-        
-        if(!token) {
-            return res.status(401).json({message: "Unauthorized denied"})
-        }
-        jwt.verify(token, variables.JWT_TOKEN, (error, user) => {
-            if(error){
-                return res.status(401).json({message: "Token invalid"})
-            }
-            req.user = user
-            next()
-        })
-    } catch(error) {
-        console.log(error);
-        return res.status(500).json({message: error.message});
+  try {
+    // Intentar obtener el token del encabezado de autorización o de las cookies
+    let token = req.headers["authorization"]
+      ? req.headers["authorization"].split(" ")[1]
+      : req.cookies["token"];
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized denied" });
     }
-}
+
+    jwt.verify(token, variables.JWT_TOKEN, (error, user) => {
+      if (error) {
+        return res.status(401).json({ message: "Token invalid" });
+      }
+      req.user = user;
+      next();
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
