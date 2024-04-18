d7d61e862926f2c399de0a98e8da14a59f21a173
David Vela <dsrv06@gmail.com>
2024-04-18 09:29:28 -0600

    changed port on app.listen

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import app from "./app.js"
 import sockets from "./sockets.js"
 import { PORT, PORTSOCKET } from "./config.js"
 
-app.listen(PORTEXPRESS)
+app.listen(PORT)
 console.log("Server is running on port:", PORT)
 
 const server = http.createServer(app)
