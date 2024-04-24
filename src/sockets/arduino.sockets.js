b1dd24174259c6d359db541d6a5233c62e0a3e84
David Vela <dsrv06@gmail.com>
2024-04-24 12:05:39 -0600

    fixed status.verify error

--- src/sockets/arduino.sockets.js
+++ src/sockets/arduino.sockets.js
@@ -18,16 +18,16 @@ export async function readUID(data) {
 
 export async function verifyUIDFromArduino(data){
     try {
-        const status = {verify: "false"}
+        const status = {verify: "false"};
         const [result] = await pool.query(
           "SELECT a.nombres, a.codigo, a.grado, a.grupo, a.carrera, a.turno, e.localizacionAlumno FROM alumnos AS a JOIN estadoAlumnos AS e ON a.UIDTarjeta = e.UIDTarjeta WHERE a.UIDTarjeta = ?;",
           [data.UID]
         );
-    
-        if (result.length == 0) {
-            const arrayTransformado = {error: "USUARIO NO ENCONTRADO"}
-            return status, arrayTransformado;
-        }
+        
+        if (result.length === 0) {
+            const arrayTransformado = [{error: "USUARIO NO ENCONTRADO"}];
+            return {status, arrayTransformado};
+        }     
         const arrayTransformado = transformarDatosArray(result);    
         console.log(arrayTransformado[0])  
         return {status, arrayTransformado}
