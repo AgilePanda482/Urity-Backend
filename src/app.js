181f9a193131f148bcdb1ff2a73cc955f46a3725
David Vela <dsrv06@gmail.com>
2024-04-24 00:14:23 -0600

    added credentials off on cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,11 @@ import { LINKFRONT } from "./config.js"
 
 const app = express()
 
-app.use(cors())
+app.use(cors(
+    {
+        credentials: false
+    }
+))
 app.use(express.json())
 app.use(cookieParser())
 
