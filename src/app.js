a578be1960f41b745e62571bb26ecead468558ce
David Vela <dsrv06@gmail.com>
2024-05-03 13:36:07 -0600

    Cors only whithout cofiguration

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import { LINKFRONT } from "./config.js"
 
 const app = express()
 
-app.use(cors({LINKFRONT}))
+app.use(cors());
 app.use(express.json())
 app.use(cookieParser())
 
--- src/index.js
+++ src/index.js
@@ -7,20 +7,12 @@ import { PORT } from "./config.js";
 
 // Crea el servidor HTTP a partir de la instancia de Express
 const server = http.createServer(app);
-
-// Inicializa Socket.IO pasando el servidor HTTP
-// y activa la compatibilidad con la versión 3 de Engine.IO
 const io = new websocketServer(server, {
     cors: {
         origin: "*",
     },
-    allowEIO3: true // Activa la compatibilidad con Engine.IO v3
 });
-
-// Aquí puedes inicializar tus sockets
 sockets(io);
-
-// Escucha en el puerto definido en tu configuración
 server.listen(PORT, () => {
     console.log("Express y Socket.IO están corriendo en el puerto:", PORT);
 });
