7ce6522f752e7d06f8d767fcb4148424acba2add
David Vela <dsrv06@gmail.com>
2024-05-03 13:52:35 -0600

    cors configuration

--- src/app.js
+++ src/app.js
@@ -8,7 +8,11 @@ import { LINKFRONT } from "./config.js"
 
 const app = express()
 
-app.use(cors());
+app.use(cors({
+    origin: LINKFRONT,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express.json())
 app.use(cookieParser())
 
